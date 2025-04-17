e7fea09693acc82e3cac611259c7b54c09803780
punit diwan <punitdiwan@gmail.com>
2025-04-17 13:12:54 +0530

    update

---
+++ Upload_Patrak_test.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+require('dotenv').config({ path: '../.env' });
+const JOB_URL = process.env.JOB_URL;
+const school = process.env.SCHOOL_ID;
+const batchId = process.env.BATCH_ID;
+const uid = process.env.JOB_ID;
+
+
+(async () => {
+
+    const now = Date.now();
+    let filename = `${uid}.xlsx`
+    console.log("now", now);
+
+
+    const key = `templates/marksheets/${school}/patrak/${filename}`;
+
+    const jobHistoryData = {
+        _school: school,
+        table: "job_history",
+        _uid: uid,
+        payload: {
+            status: true,
+            file_path: key  // Assuming file_path is returned from the first API call
+        }
+    };
+    const updateResponse = await axios.post(JOB_URL, jobHistoryData, {
+        headers: {
+            'Content-Type': 'application/json',  // Make sure the content type is application/json for API update
+        }
+    });
+    console.log('Job history updated:', updateResponse.data);
+})();
